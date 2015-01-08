7826472793e3e3b619ef192faed9a089224a77e6
Vognik <svognik@gmail.com>
2015-01-08 21:37:23 +0200

    breakdown

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,11 +1,3 @@
-$("header nav ul li").click(function () {
-	$("header nav ul li").removeClass("active");
-	$(this).addClass("active");
-	$("section").hide();
-	var activeSection = $(this).attr("name");
-	$("#" + activeSection).show();
-});
-
 $("section nav ul li").click(function () {
 	$("section nav ul li").removeClass("active-app");
 	$(this).addClass("active-app");
